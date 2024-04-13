1d58a46af38c3a7ba6da75f36591630082ae4009
mohamed abdou <modohoda2468@gmail.com>
2024-04-13 13:58:06 +0200

    Add project files.

---
+++ Models/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApi.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        [Required]
+        [MinLength(3)]
+        public string Name { get; set; }
+        [Required]
+        [Range(1000,10000)]
+        public int Salary { get; set; }
+        public string Address { get; set; }
+
+        public int Age { get; set; }    
+    }
+}
---
+++ Models/ITIEntity.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApi.Models
+{
+    public class ITIEntity: DbContext
+    {
+        public ITIEntity() { }
+        public ITIEntity(DbContextOptions options):base(options) { }
+
+        public DbSet<Employee> Employees { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=.;Initial Catalog=WebApi;Integrated Security=True;Encrypt=False;");
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
